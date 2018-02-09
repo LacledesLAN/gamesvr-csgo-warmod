3b3119f57b9c4bae104bca8fda20d0cea7abfd70
James Dudley <james@dudley.codes>
2018-02-08 21:39:09 -0600

    Updating test script

--- dist.linux/ll-tests/gamesvr-csgo-tourney.sh
+++ dist.linux/ll-tests/gamesvr-csgo-tourney.sh
@@ -1,55 +1,87 @@
 #!/bin/bash
 
-declare LLTEST_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-declare LLTEST_RESULTS="";
-declare LLTEST_HASFAILS=false;
-declare LLTEST_LOGFILE="$LLTEST_SCRIPTDIR/gamesvr-csgo-tourney.log";
-declare LLTEST_COMMAND="$LLTEST_SCRIPTDIR/../srcds_run -game csgo +game_type 0 +game_mode 1 -console +map de_nuke";
+#!/bin/bash -i
+declare LLCK_LOGFILE="/app/ll-tests/gamesvr-csgo-tourney.log";
+declare LLCK_CMD="/app/srcds_run -game csgo +game_type 0 +game_mode 1 -console +map de_nuke -norestart -insercure +sv_lan 1";
+declare LLCK_CMD_TIMEOUT=60;
 
+: > $LLCK_LOGFILE
+if [ ! -f $LLCK_LOGFILE ]; then
+    echo 'Failed to create logfile: '"$LLCK_LOGFILE"'. Verify file system permissions.';
+    exit 1;
+fi;
 
-# $1 -> text you want to find
+# $1 -> text we want to find in the log file
 # $2 -> description of why we want text to exist
-function should_have() { 
-    if ! grep -i -q "$1" "$LLTEST_LOGFILE"; then
-        LLTEST_RESULTS+=$'\nFAIL: '"$2";
-        LLTEST_HASFAILS=true;
+function should_have() {
+    if ! grep -i -q "$1" "$LLCK_LOGFILE"; then
+        echo $'FAIL: '"$2";
+        LLCK_HASFAILS=true;
     else
-        LLTEST_RESULTS+=$'\nPASS: '"$2";
+        echo $'PASS: '"$2";
     fi;
 }
 
-
-# $1 -> text you don't want to find
+# $1 -> text we *don't* want to find in the log file
 # $2 -> description of why we don't want text to exist
 function should_lack() {
-    if grep -i -q "$1" "$LLTEST_LOGFILE"; then
-        LLTEST_RESULTS+=$'\n FAIL: '"$2";
-        LLTEST_HASFAILS=true;
+    if grep -i -q "$1" "$LLCK_LOGFILE"; then
+        echo $'FAIL: '"$2";
+        LLCK_HASFAILS=true;
     else
-        LLTEST_RESULTS+=$'\nPASS: '"$2";
+        echo $'PASS: '"$2";
     fi;
     return 0;
 }
 
+declare LLCK_HASFAILS=false;
 
-if [ -f "$LLTEST_SCRIPTDIR/../srcds_run" ]; then
-    echo "###################################################################";
-    echo $'Attemping to run server for 90 seconds to capture output';
-    echo $'Command: '"$LLTEST_COMMAND";
-    echo "Running as $(id)";
-    echo "###################################################################";
-    $LLTEST_COMMAND > "$LLTEST_LOGFILE" 2>&1 &
-    pid=$!
-    #kill pid NOT NEEDED IN DOCKER BUT SHOULD BE FLUSHED OUT; WOULD ALLOW USAGE OUTSIDE CONAINTER CONTEXT
-    sleep 90
+echo $'\n\n[RUNNING APP FOR '"$LLCK_CMD_TIMEOUT SECONDS]";
+echo $'Command: '"$LLCK_CMD";
+echo "Running under $(id)";
+
+SCREEN_RCFILE=$(mktemp);
+echo "logfile $LLCK_LOGFILE" > "$SCREEN_RCFILE";
+
+SCREEN_ID=LLCHECKS.$$.$RANDOM;
+# shellcheck disable=SC2086
+screen -c "$SCREEN_RCFILE" -m -d -L -S "$SCREEN_ID" $LLCK_CMD
+
+SCREEN_PID="$(screen -ls | grep $SCREEN_ID | awk -F . '{print $1}' | awk '{print $1}')";
+unset SCREEN_ID;
+
+if [[ ! -z "${SCREEN_PID// }" ]]; then      # Make sure screen session isn't already terminated
+    # shellcheck disable=SC2086
+    while ps -p $SCREEN_PID > /dev/null; do
+        if [ $LLCK_CMD_TIMEOUT -le 0 ]; then
+            kill -9 -"$SCREEN_PID";
+            echo "done.";
+            break;
+        fi;
+        if (( LLCK_CMD_TIMEOUT % 5 == 0 )); then
+            echo -n "$LLCK_CMD_TIMEOUT...";
+        fi;
+        (( LLCK_CMD_TIMEOUT-- ))
+        sleep 1;
+    done
+    unset LLCK_CMD_TIMEOUT;
+fi;
+
+rm -f "$SCREEN_RCFILE"; unset SCREEN_RCFILE;
+
+screen -wipe > /dev/null;
+
+if [ -s $LLCK_LOGFILE ]; then               # Make sure log file isn't empty
+    echo $'\n[LOGFILE OUTPUT]';
+    cat $LLCK_LOGFILE;
 else
-    echo "srcds_run not found!";
-    echo "Test script currently only works inside of the docker container";
+    echo $'\n\nOUTPUT LOG IS EMPTY!\n\n';
     exit 1;
 fi;
 
+echo $'\n[RUNNING CHECKS ON LOGFILE]';
 
-should_have 'Server will auto-restart if there is a crash' 'Sever started executing';
+should_have 'Setting breakpad minidump AppID = 740' 'Sever started executing';
 should_lack 'Server restart in 10 seconds' 'Server is not boot-looping';
 should_lack 'Running the dedicated server as root' 'Server is not running under root';
 should_have 'Game.dll loaded for "Counter-Strike: Global Offensive"' 'srcds_run loaded CSGO';
@@ -68,25 +100,15 @@ should_have 'Admin File Reader' 'LL status mod reports admin file reader';
 should_have 'Basic Info Triggers' 'LL status mod reports basic info triggers';
 should_have 'Basic Comm Control' 'LL status mod reports basic comm control';
 should_have 'Anti-Flood' "LL status mod reports anti-flood";
+#should_lack 'Successfully updated gamedata file "' 'SourceMod is not self updating'
+#should_lack 'SourceMod has been updated, please reload it or restart your server' 'SourceMod is not requesting restart'
 
-echo "###################################################################";
-echo "                          CAPTURED OUTPUT                          ";
-echo "###################################################################";
+echo $'\n\n[FINAL RESULT]';
 
-cat "$LLTEST_LOGFILE";
-echo $'\n';
-
-echo "##################################################################";
-echo "                           TEST RESULTS                           "
-echo "##################################################################";
-
-echo "$LLTEST_RESULTS";
-echo $'\n';
-
-if [ $LLTEST_HASFAILS = true ]; then
-    echo $'\nTests has errors!';
+if [ $LLCK_HASFAILS = true ]; then
+    echo $'Checks have failures!\n\n\n';
     exit 1;
 fi;
 
-echo 'All tests passed';
+echo $'All checks passed!\n\n\n';
 exit 0;
